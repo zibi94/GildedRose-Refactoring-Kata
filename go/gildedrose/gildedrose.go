16d9ab103c4d05adda89a635ba7f375b1426b999
Zbyszek Tobiasz <zbyszek.tobiasz@smartcommerce.net>
2024-05-27 09:07:46 +0200

    refactor code; use separated handlers

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -1,58 +1,104 @@
 package gildedrose
 
+const (
+	// Quality extremes.
+	MaxQuality = 50
+	MinQuality = 0
+
+	// Items which need custom quality handling.
+	AgedBrie        = "Aged Brie"
+	BackstagePasses = "Backstage passes to a TAFKAL80ETC concert"
+	Sulfuras        = "Sulfuras, Hand of Ragnaros"
+	Conjured        = "Conjured Mana Cake" // New item.
+)
+
 type Item struct {
 	Name            string
 	SellIn, Quality int
 }
 
+// The method sets the minimum and maximum quality if the value exceeds the extreme values.
+func (i *Item) alignQuality() {
+	if i.Quality < MinQuality {
+		i.Quality = MinQuality
+	}
+	if i.Quality > MaxQuality {
+		i.Quality = MaxQuality
+	}
+}
+
 func UpdateQuality(items []*Item) {
-	for i := 0; i < len(items); i++ {
-
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
-				}
-			}
+	for i := range items {
+		if fn, ok := handlers[items[i].Name]; ok {
+			fn(items[i])
 		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-				}
-			}
-		}
-
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
-		}
-
-		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
-						}
-					}
-				} else {
-					items[i].Quality = items[i].Quality - items[i].Quality
-				}
-			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
-				}
-			}
+			defaultHandler(items[i])
 		}
 	}
 
+}
+
+// Custom handlers.
+var handlers = map[string]func(i *Item){
+	AgedBrie: func(i *Item) {
+		i.SellIn--
+		i.Quality++
+		i.alignQuality()
+	},
+
+	BackstagePasses: func(i *Item) {
+		i.SellIn -= 1
+		if i.Quality == 0 {
+			return
+		}
+
+		if i.SellIn <= 0 {
+			i.Quality = 0
+			return
+		}
+
+		if i.SellIn <= 10 {
+			i.Quality++
+		}
+		if i.SellIn <= 5 {
+			i.Quality++
+		}
+
+		i.Quality++
+		i.alignQuality()
+	},
+
+	Sulfuras: func(i *Item) {
+		// Legendary item - never has to be sold or decreases in Quality.
+	},
+
+	Conjured: func(i *Item) {
+		i.SellIn--
+		if i.Quality == 0 {
+			return
+		}
+
+		i.Quality -= 2
+		i.alignQuality()
+	},
+}
+
+// Default handler.
+func defaultHandler(i *Item) {
+	// Fix foo item name.
+	if i.Name == "foo" {
+		i.Name = "fixme"
+	}
+
+	i.SellIn -= 1
+	if i.Quality == 0 {
+		return
+	}
+
+	if i.SellIn <= 0 {
+		i.Quality--
+	}
+
+	i.Quality--
+	i.alignQuality()
 }
